db43663d78d755ccb5b2e76ee0700b2f5c0839fa
lifeIP <molokeevartem@gmail.com>
2025-04-06 06:06:09 +0300

    В статистику добавлен график

--- src/Pages/Profile/ProfilePage.js
+++ src/Pages/Profile/ProfilePage.js
@@ -1,6 +1,7 @@
-import { Box, Card, CardMedia, CardContent, Grid, Grid2, Typography } from '@mui/material'
+import { Box, Card, CardMedia, CardContent, Grid, Grid2, Typography, Paper } from '@mui/material'
 import Skeleton from '@mui/material/Skeleton';
 import { useState } from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
 import React, { useEffect } from 'react';
 import axios from 'axios'
 import settings from "../../settings.json"
@@ -47,19 +48,38 @@ function Profile() {
 }
 
 function Statistics() {
+    const [is_load, setLoad] = useState(true)
     return (
         <Card sx={{ borderRadius: "12px", width: "30vw", height: "50vh" }}>
 
             <CardContent>
-                <Typography gutterBottom variant="h3" component="div" textAlign="center">
-                    Статистика
+                <Typography gutterBottom variant="h4" component="div" textAlign="center">
+                Ресурсы
                 </Typography>
+
             </CardContent>
             <CardContent>
-                <Typography gutterBottom variant="body1" component="div">
-                    <Skeleton animation="wave" />
-                </Typography>
+               
+
+                {!is_load ? (
+                    <Skeleton sx={{ height: 235}} animation="wave" variant="rectangular" />
+                ) : (
+                    <PieChart
+                        series={[
+                            {
+                                data: [
+                                    { id: 0, value: 10, label: 'M124' },
+                                    { id: 1, value: 15, label: 'series B' },
+                                    { id: 2, value: 20, label: 'series C' },
+                                ],
+                            },
+                        ]}
+                        width={400}
+                        height={235}
+                    />
+                )}
             </CardContent>
+
         </Card>
 
     );
