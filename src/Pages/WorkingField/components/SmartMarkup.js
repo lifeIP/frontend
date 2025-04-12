39e167a0d41d0003d45549c721b07c4cfd62e5d2
lifeIP <molokeevartem@gmail.com>
2025-04-12 23:11:10 +0300

    Найдена возможность остановки перехвата управления TransformWrapper

--- src/Pages/WorkingField/components/SmartMarkup.js
+++ src/Pages/WorkingField/components/SmartMarkup.js
@@ -14,38 +14,40 @@ export default function SmartMarkup() {
     const CanvasOverImage = () => {
         const imageRef = useRef(null);
         const canvasRef = useRef(null);
-        
+
         const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-        const [canvasSize, setCanvasSize] = useState({width: 2000, height: 2000});
-    
+        const [canvasSize, setCanvasSize] = useState({ width: 2000, height: 2000 });
+
         const handleMouseMove = (event) => {
             if (!canvasRef.current) return;
-    
+
             const rect = canvasRef.current.getBoundingClientRect();
             const relativeX = event.clientX - rect.left;
             const relativeY = event.clientY - rect.top;
-    
-            if (relativeX > 0 && relativeX < canvasSize.width && relativeY > 0 && relativeY < canvasSize.height){
-                setMousePosition({ x: relativeX, y: relativeY });    
+
+            if (relativeX > 0 && relativeX < canvasSize.width && relativeY > 0 && relativeY < canvasSize.height) {
+                setMousePosition({ x: relativeX, y: relativeY });
             }
-            
+
         };
-    
-    
-    
-    
-        
-    
+
+
+
+
+
+
+
+
         // Функция для рисования на канвасе
         const drawCanvas = () => {
             if (canvasRef.current) {
                 const context = canvasRef.current.getContext('2d');
-    
+
                 const path1 = new Path2D();     // первый путь
                 context.lineWidth = 3;
                 path1.moveTo(20, 20);
                 path1.lineTo(250, 20);
-                
+
                 path1.lineTo(250, 250);
                 path1.lineTo(20, 250);
                 path1.closePath();    //  закрываем путь
@@ -53,26 +55,43 @@ export default function SmartMarkup() {
                 context.stroke(path1);
             }
         };
-    
+
         // Эффект для инициализации канваса после рендера
         useEffect(() => {
             drawCanvas();
-    
-    
+
+
+
+            // document.addEventListener('mousedown', function (event) {
+            //     if (event.button === 0) { // Левая кнопка мыши
+            //         leftButtonPressed = true;
+            //         console.log('Левая кнопка нажата');
+            //     }
+            // });
+
+            // document.addEventListener('mouseup', function (event) {
+            //     if (leftButtonPressed && event.button === 0) { // Левая кнопка мыши
+            //         leftButtonPressed = false;
+            //         console.log('Левая кнопка отпущена');
+            //     }
+            // });
+
+
+
             document.addEventListener('mousemove', handleMouseMove);
             return () => {
                 document.removeEventListener('mousemove', handleMouseMove);
             };
         }, [canvasSize]);
-    
+
         return (
             <Box sx={{ position: 'relative', width: '100%', height: 'auto' }}>
-                <Card sx={{ width: "51.05vw"}}>
+                <Card sx={{ width: "51.05vw" }}>
                     <CardMedia
                         ref={imageRef}
                         component="img"
-                        onLoad={()=>{
-                            setCanvasSize({width: imageRef.current.width, height: imageRef.current.height});
+                        onLoad={() => {
+                            setCanvasSize({ width: imageRef.current.width, height: imageRef.current.height });
                         }}
                         image={"https://avatars.mds.yandex.net/i?id=f7454bc3badcefdfbef33cfd38fdc121_l-5194719-images-thumbs&n=13"}
                         alt="Фотография"
@@ -85,7 +104,7 @@ export default function SmartMarkup() {
                         }}
                     />
                 </Card>
-    
+
                 <canvas
                     ref={canvasRef}
                     width={canvasSize.width}
@@ -134,11 +153,11 @@ export default function SmartMarkup() {
     return (
         <Card sx={{ width: "51.05vw" }}>
             <Box>
-            <TransformWrapper >
-                <TransformComponent>
-                    <CanvasOverImage />
-                </TransformComponent>
-            </TransformWrapper>
+                <TransformWrapper disabled={true}>
+                    <TransformComponent>
+                        <CanvasOverImage />
+                    </TransformComponent>
+                </TransformWrapper>
             </Box>
             <Actions />
         </Card>
