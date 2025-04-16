8ca8d2555d62ab3245702fac872d308587d2ff79
lifeIP <molokeevartem@gmail.com>
2025-04-16 22:12:02 +0300

    Добавлена шапка страницы проекта

--- src/Pages/Project/ProjectPage/ProjectPage.js
+++ src/Pages/Project/ProjectPage/ProjectPage.js
@@ -1,10 +1,17 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import Center from '../../../components/Center/Center';
+import Hat from '../../../components/Hat/Hat';
+
 
 export default function ProjectPage() {
     return (
-        <Box>
-            <Typography variant='h1'>ProjectPage</Typography>
-        </Box>
+        <Center>
+            <Hat>
+                <Typography gutterBottom variant="h3" component="div" textAlign="center">
+                    Проект
+                </Typography>
+            </Hat>
+        </Center>
     );
 }
