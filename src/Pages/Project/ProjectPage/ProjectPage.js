d716728bf760f88392ab585d5c2338066b56907d
lifeIP <molokeevartem@gmail.com>
2025-04-23 13:55:51 +0700

    Начата разработка миниатюры фотографии

--- src/Pages/Project/ProjectPage/ProjectPage.js
+++ src/Pages/Project/ProjectPage/ProjectPage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Center from '../../../components/Center/Center';
 import Hat from '../../../components/Hat/Hat';
 import ProjectCardPreview from '../CreateProject/components/ProjectCardPreview';
+import ImageViewer from './components/ImageViewer'
 
 export default function ProjectPage() {
     const [projectId, setProjectId] = useState(0);
@@ -12,11 +13,11 @@ export default function ProjectPage() {
     const [prjctDescription, setPrjctDescription] = useState("Краткое описание проекта, оно не должно превышать определённого количества символов.");
     const [rows, setRows] = useState([]);
 
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         setProjectId(localStorage.getItem("last_project_id"));
     }, []);
-        
+
     return (
         <Center>
             <Hat>
@@ -24,7 +25,7 @@ export default function ProjectPage() {
                     Проект {projectId}
                 </Typography>
             </Hat>
-        
+
             <Grid container spacing={1}>
                 <Grid size={4.88}>
                     <ProjectCardPreview
@@ -36,13 +37,85 @@ export default function ProjectPage() {
                 </Grid>
             </Grid>
 
-            <Card sx={{ borderRadius: "12px", width: "51.05vw", minHeight: "100px", marginTop: '1.85vh', marginBottom: '1.85vh' }}>
-                    <CardContent>
-                        <Typography gutterBottom variant="h3" component="div" textAlign="center">
-                            Фотографии
-                        </Typography>
-                    </CardContent>
+            <Card sx={{ borderRadius: "12px", width: "51.05vw", minHeight: "100px", marginTop: '1.85vh' }}>
+                <CardContent>
+                    <Typography gutterBottom variant="h3" component="div" textAlign="center">
+                        Фотографии
+                    </Typography>
+                </CardContent>
+
             </Card>
+
+            <Box sx={{ width: "51.05vw" }}>
+                <Grid container spacing={1} sx={{ marginTop: '1vh' }}>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                </Grid>
+                <Grid container spacing={1} sx={{ marginTop: '1vh' }}>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                </Grid>
+                <Grid container spacing={1} sx={{ marginTop: '1vh' }}>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                </Grid>
+                <Grid container spacing={1} sx={{ marginTop: '1vh' }}>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                    <Grid size={3}>
+                        <ImageViewer setCanvasSize={() => { }} />
+
+                    </Grid>
+                </Grid>
+            </Box>
         </Center>
     );
 }
---
+++ src/Pages/Project/ProjectPage/components/ImageViewer.js
@@ -0,0 +1,105 @@
+import { Box, Card, CardMedia } from "@mui/material";
+import settings from "../../../../settings.json"
+import axios from 'axios';
+import React, {
+    useEffect,
+    useRef,
+    useState
+} from 'react';
+
+
+
+
+const ImageViewer = () => {
+    const canvasRef = useRef(null);
+    const [canvasSize, setCanvasSize] = useState({ width: 2000, height: 2000 });
+
+    const imageRef = useRef(null);
+    // const [isLoading, setLoading] = useState(true);
+    const [image, setImage] = useState();
+
+
+    const drawCanvas = () => {
+        if (canvasRef.current) {
+            const context = canvasRef.current.getContext('2d');
+
+            context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            context.lineWidth = 3;
+
+            const path1 = new Path2D();
+            context.strokeStyle = "#FF0000";
+            path1.rect(20, 20, 100, 80)
+            path1.closePath();    //  закрываем путь
+            context.stroke(path1);
+        }
+    };
+
+
+    useEffect(() => {
+        // if(isLoading!=false){
+
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization")
+        axios.get(`${settings.server.addr}/get-image-by-id/${1}?t=${Date.now()}`, {
+            responseType: "arraybuffer"
+        })
+            .then(res => {
+                const base64 = btoa(
+                    new Uint8Array(res.data).reduce(
+                        (data, byte) => data + String.fromCharCode(byte),
+                        ''
+                    )
+                )
+                setImage(`data:image/jpeg;charset=utf-8;base64,${base64}`);
+                // setLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        // }
+        drawCanvas();
+    }, []);
+
+    return (
+        <Box position="relative">
+            <canvas
+                ref={canvasRef}
+                width={canvasSize.width}
+                height={canvasSize.height}
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 15,
+                }}
+            />
+            <Card>
+                <CardMedia
+                    key={image}
+                    ref={imageRef}
+                    component="img"
+                    onLoad={() => {
+                        setCanvasSize({
+                            width: imageRef.current.width,
+                            height: imageRef.current.height
+                        });
+                    }}
+                    src={image}
+                    alt="Фотография"
+                    sx={{
+                        width: '100%',   // Ширина фотографии — 100% ширины контейнера
+                        height: '100%',  // Высота автоматически адаптируется под ширину
+                        objectFit: 'contain',  // Подгоняем картинку без искажений
+                        display: 'block',
+                        margin: '0 auto'  // Центрируем изображение горизонтально
+                    }}
+                />
+
+            </Card>
+        </Box>
+    );
+}
+
+
+export default ImageViewer;
