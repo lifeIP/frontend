25ee597b3c423709acbbd6839ac3c85ce4bd8f6b
lifeIP <molokeevartem@gmail.com>
2025-05-04 01:37:18 +0700

    Добавлена кнопка настроек

--- src/Pages/Project/CreateProject/components/ProjectCardPreview.js
+++ src/Pages/Project/CreateProject/components/ProjectCardPreview.js
@@ -1,9 +1,9 @@
-import { Card, CardActionArea, CardContent, CardMedia, Divider, Typography } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Divider, IconButton, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
+import SettingsIcon from '@mui/icons-material/Settings';
 
-
-function ProjectCardPreview({ isImage, prjctName, prjctDescription, rows, actionAreaDisabled=true, onClick=()=>{} }) {
+function ProjectCardPreview({ isImage, prjctName, prjctDescription, rows, actionAreaDisabled = true, onClick = () => { } }) {
     const [image, setImage] = useState("https://kuzov73.ru/image/cache/no_image-1000x1000.jpg");
     useEffect(() => {
         if (isImage !== undefined) {
@@ -15,58 +15,72 @@ function ProjectCardPreview({ isImage, prjctName, prjctDescription, rows, action
 
 
     return (
-        <Card sx={{ width: "25vw", borderRadius: "12px", height: "45vh" }}>
-            <CardActionArea sx={{ width: "25vw", height: "45vh" }} disabled={actionAreaDisabled} onClick={()=>{onClick()}}>
-            <CardMedia
-                sx={{ height: "25vh" }}
-                component="img"
-                height="250px"
-                src={image}
-                alt="green iguana"
-            />
-            <CardContent>
-                <Typography
-                    sx={{
-                        display: '-webkit-box',
-                        overflow: 'hidden',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 1,
-                    }}
-                    gutterBottom 
-                    variant="h5" 
-                    component="div" >
-                    {prjctName}
-                </Typography>
-                <Typography
-                    sx={{
-                        color: 'text.secondary',
-                        display: '-webkit-box',
-                        overflow: 'hidden',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 3,
-                    }}
-                    overflow={"hidden"}
-                    variant="body2"
-                    textAlign="justify">
-                    {prjctDescription}
-                </Typography>
+        <Card sx={{ width: "25vw", borderRadius: "12px", height: "45vh", position: "relative" }}>
+            <CardActionArea sx={{ width: "25vw", height: "45vh" }} disabled={actionAreaDisabled} onClick={() => { onClick() }}>
+                <CardMedia
+                    sx={{ height: "25vh" }}
+                    component="img"
+                    height="250px"
+                    src={image}
+                    alt="image"
+                />
+                <CardContent>
+                    <Typography
+                        sx={{
+                            display: '-webkit-box',
+                            overflow: 'hidden',
+                            WebkitBoxOrient: 'vertical',
+                            WebkitLineClamp: 1,
+                        }}
+                        gutterBottom
+                        variant="h5"
+                        component="div" >
+                        {prjctName}
+                    </Typography>
+                    <Typography
+                        sx={{
+                            color: 'text.secondary',
+                            display: '-webkit-box',
+                            overflow: 'hidden',
+                            WebkitBoxOrient: 'vertical',
+                            WebkitLineClamp: 3,
+                        }}
+                        overflow={"hidden"}
+                        variant="body2"
+                        textAlign="justify">
+                        {prjctDescription}
+                    </Typography>
 
-                <Typography
-                    sx={{
-                        color: 'text.secondary',
-                        display: '-webkit-box',
-                        overflow: 'hidden',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 1,
-                        color: 'text.secondary',
-                        marginTop: "10px"
-                    }}
-                    variant="body2">
-                    <Divider />
-                    {rows.map((item) => `${item.label}`).join(', ')}
-                </Typography>
-            </CardContent>
+                    <Typography
+                        sx={{
+                            color: 'text.secondary',
+                            display: '-webkit-box',
+                            overflow: 'hidden',
+                            WebkitBoxOrient: 'vertical',
+                            WebkitLineClamp: 1,
+                            color: 'text.secondary',
+                            marginTop: "10px"
+                        }}
+                        variant="body2">
+                        <Divider />
+                        {rows.map((item) => `${item.label}`).join(', ')}
+                    </Typography>
+                </CardContent>
             </CardActionArea>
+
+            {
+                actionAreaDisabled?(
+                <IconButton
+                    sx={{ position: 'absolute', top: 8, right: 8 }}
+                    aria-label="settings"
+                    onClick={() => console.log('Settings clicked')}
+                >
+                    <SettingsIcon />
+                </IconButton>
+                ):(
+                    <div></div>
+                )
+            }
         </Card>
     );
 }
--- src/Pages/Project/ProjectPage/components/ListOfMembers.js
+++ src/Pages/Project/ProjectPage/components/ListOfMembers.js
@@ -1,12 +1,15 @@
-import { Card } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
 
 
 function ListOfMembers() {
-
-
     return (
         <Card sx={{ width: "100%", borderRadius: "12px", height: "45vh" }}>
+            <CardContent>
+                <Typography>
+                    Список участников
+                </Typography>
+            </CardContent>
         </Card>
     );
 }
