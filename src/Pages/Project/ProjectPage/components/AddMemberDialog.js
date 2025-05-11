0909cf942a73a53283385ce7cd214548ef9236bd
lifeIP <molokeevartem@gmail.com>
2025-05-11 16:30:48 +0700

    Добавлена валидация для email

--- src/Pages/Project/ProjectPage/components/AddMemberDialog.js
+++ src/Pages/Project/ProjectPage/components/AddMemberDialog.js
@@ -1,32 +1,68 @@
 import * as React from 'react';
-import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, Fab } from '@mui/material';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, FormHelperText, Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-
 
 export default function AddMemberDialog() {
   const [open, setOpen] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [isValidEmail, setIsValidEmail] = React.useState(true);
+
+  // Регулярное выражение для проверки email
+  const validateEmail = (value) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(value.trim());
+  };
+
+  const handleSubmit = () => {
+    if (validateEmail(email)) {
+      console.log(`Отправлен новый email: ${email}`);
+      setOpen(false);
+      setEmail('');
+    } else {
+      setIsValidEmail(false);
+    }
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+    setIsValidEmail(validateEmail(event.target.value));
+  };
+
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
+    setEmail('');
+    setIsValidEmail(true);
   };
 
   return (
     <>
-      <Fab color="primary" size="large" aria-label="добавить участник" onClick={handleClickOpen}>
-          <AddIcon />
+      <Fab color="primary" size="large" aria-label="Добавить участника" onClick={handleClickOpen}>
+        <AddIcon />
       </Fab>
-      
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Поисковая строка</DialogTitle>
+      <Dialog fullWidth open={open} onClose={handleClose}>
+        <DialogTitle>Добавить участника</DialogTitle>
         <DialogContent>
-          <TextField autoFocus margin="dense" id="search-input" label="Введите запрос..." fullWidth />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="search-input"
+            label="Email"
+            error={!isValidEmail && !!email.length > 0}
+            helperText={!isValidEmail ? 'Некорректный email!' : ''}
+            fullWidth
+            value={email}
+            onChange={handleChangeEmail}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Отмена</Button>
-          <Button color="primary">Искать</Button>
+          <Button disabled={!isValidEmail} color="primary" onClick={handleSubmit}>
+            Искать
+          </Button>
         </DialogActions>
       </Dialog>
     </>
--- src/Pages/Project/ProjectPage/components/ListOfMembers.js
+++ src/Pages/Project/ProjectPage/components/ListOfMembers.js
@@ -4,6 +4,28 @@ import AddIcon from '@mui/icons-material/Add';
 import AddMemberDialog from './AddMemberDialog';
 
 function ListOfMembers() {
+
+    // async function getListOfMembers() {
+    //     let url = "/get-projects-info-by-id/" + projectId;
+
+    //     try {
+    //         axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization")
+    //         const res = await axios.get(`${settings.server.addr}${url}`);
+
+    //         if (res.status === 200 || res.status === 201) {
+    //             // setListProjects(res.data);
+    //             console.log(res.data);
+    //             setPrjctName(res.data.name);
+    //             setPrjctDescription(res.data.description);
+    //         } else {
+    //             throw new Error('Ошибка при отправке даннх');
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //         throw err;
+    //     }
+    // }
+
     return (
         <Card sx={{ width: "100%", borderRadius: "12px", height: "45vh" }}>
             <Box
@@ -15,12 +37,12 @@ function ListOfMembers() {
                 }}
             >
                 <Box >
-                <Typography>
-                    В проекте пока нет сторонних участников
-                </Typography>
-                <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                <AddMemberDialog/>
-                </Box>
+                    <Typography>
+                        В проекте пока нет сторонних участников
+                    </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <AddMemberDialog />
+                    </Box>
                 </Box>
             </Box>
         </Card>
