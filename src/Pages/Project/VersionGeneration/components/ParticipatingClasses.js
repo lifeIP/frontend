84caca8de2cbd472445f916dba2686dfe4a07ea9
lifeIP <molokeevartem@gmail.com>
2025-07-20 15:59:00 +0700

    Работа над графическим отображением классов в компоненте 'Участвующие классы' закрнчена

--- src/Pages/Project/VersionGeneration/components/ParticipatingClasses.js
+++ src/Pages/Project/VersionGeneration/components/ParticipatingClasses.js
@@ -1,4 +1,18 @@
-import { Box, Button, Card, CardContent, Checkbox, IconButton, List, ListItem, ListItemText, ListSubheader, Skeleton, Typography } from '@mui/material';
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Checkbox,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    ListSubheader,
+    Skeleton,
+    Typography
+} from '@mui/material';
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import settings from "../../../../settings.json"
@@ -10,112 +24,127 @@ import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined
 import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
 
 // get_list_of_classes_in_project
-function SomeClass({ id, class_name, class_color, class_description }) {
-    const [countOfPhoto, setCountOfPhoto] = useState(586);
-    const [countOfObject, setCountOfObject] = useState(11);
-    async function getCount(project_id, class_id) {
-        let url = "/get_count_of_objects_in_class/" + project_id + "/" + class_id;
+function SomeClass({ id, class_name, class_color, class_description, class_count, class_photo_count, hideClass, setHideClass }) {
+    const [checked, setChecked] = React.useState(true);
 
-        try {
-            axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization")
-            const res = await axios.get(`${settings.server.addr}${url}`);
-
-            if (res.status === 200 || res.status === 201) {
-                setCountOfObject(res.data.count_object);
-            } else {
-            }
-        } catch (err) {
-            // console.error(err);
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+        if (event.target.checked == false) {
+            setHideClass([...hideClass, id]);
         }
-    }
+        else {
+            setHideClass(hideClass.filter((item) => item !== id));
+        }
+    };
 
-    useEffect(() => {
-        getCount(localStorage.getItem("last_project_id"), id);
-    }, []);
     return (
         <>
             <ListItemText
                 primary={
                     <>
                         <Box sx={{ display: "flex" }}>
-                            <Typography sx={{ marginRight: "4px" }} variant="subtitle1" color="text.primary">Имя класса:</Typography>
-                            <Typography sx={{ minWidth: "14%" }} variant="subtitle1" fontWeight="bold" color="text.primary">
+                            <Typography
+                                sx={{
+                                    marginRight: "4px"
+                                }}
+                                variant="subtitle1"
+                                color="text.primary">
+                                Имя класса:
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    minWidth: "14%"
+                                }}
+                                variant="subtitle1"
+                                fontWeight="bold"
+                                color="text.primary">
                                 {class_name}
                             </Typography>
 
-                            <Typography sx={{ marginRight: "4px" }} variant="subtitle1">Цвет:</Typography>
-                            <Typography color={class_color} variant="subtitle1">
+                            <Typography
+                                sx={{ marginRight: "4px" }}
+                                variant="subtitle1">
+                                Цвет:
+                            </Typography>
+                            <Typography
+                                color={class_color}
+                                variant="subtitle1">
                                 {class_color}
                             </Typography>
                         </Box>
                     </>
                 }
                 secondary={
-                    <Box>
+                    <Box
+                        sx={{
+                            minWidth: "40vw"
+                        }}>
                         <Typography variant="body2" color="text.secondary" >
                             {class_description != "" ? class_description : "Нет описания"}
                         </Typography>
                         <Box sx={{ marginTop: "3px", display: "flex" }}>
-                            <Typography marginRight="15px" variant="body2" fontWeight="bold" color="text.secondary">Фотографий: {countOfPhoto}</Typography>
-                            <Typography variant="body2" fontWeight="bold" color="text.secondary">Объектов: {countOfObject}</Typography>
+                            {/* <Typography
+                                marginRight="15px"
+                                variant="body2"
+                                fontWeight="bold"
+                                color="text.secondary">
+                                Фотографий: {class_photo_count}
+                            </Typography> */}
+                            <Typography
+                                variant="body2"
+                                fontWeight="bold"
+                                color="text.secondary">
+                                Объектов: {class_count}
+                            </Typography>
                         </Box>
                     </Box>
                 }
             />
             <ListSubheader>
-                <Checkbox defaultChecked />
+                <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                />
             </ListSubheader>
         </>
     );
 }
 
 
-function SomePie() {
-    const [is_load, setLoad] = useState(true);
+function SomePie({ listOfClasses, hideClass, is_load }) {
+
     const [series, setSeries] = useState([
         {
             data: [
-                { value: 100, label: "Глаз" },
-                { value: 121, label: "Рот" },
-                { value: 151, label: "Нос" },
-                { value: 10, label: "Ебало" },
-                { value: 113, label: "Хлебало" },
-                { value: 113, label: "Флюгенгехаймен" },
-                { value: 10, label: "Ебало 2" },
-                { value: 113, label: "Хлебало 2" },
             ],
         },
     ]);
+
+
+
     const otherProps = {
         width: 450,
         height: 250,
     };
 
 
-    async function getSeries(project_id) {
-        let url = "/get_series_for_pie/" + project_id;
-
-        try {
-            axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization")
-            const res = await axios.get(`${settings.server.addr}${url}`);
-
-            if (res.status === 200 || res.status === 201) {
-                // setSeries(res.data.series);
-                console.log(res.data)
-                let data = []
-                res.data.map((item)=>{
-                    data.push({value: 25, label: item.class_name, color: item.class_color});
-                });
-                setSeries([{data: data}]);
-            } else {
-            }
-        } catch (err) {
-            // console.error(err);
-        }
-    }
     useEffect(() => {
-        getSeries(localStorage.getItem("last_project_id"));
-    }, []);
+        let data = []
+        console.log(hideClass)
+        console.log(listOfClasses)
+        listOfClasses.map((item) => {
+            if(hideClass?.filter((item2) => item2 == item.class_id).length == 0){
+            data.push(
+                {
+                    value: item.class_count,
+                    label: item.class_name,
+                    color: item.class_color
+                });
+            }
+
+        });
+        setSeries([{ data: data }]);
+    }, [hideClass, listOfClasses]);
 
     return (
         <Box sx={{ height: 300, width: 350, display: "flex", justifyContent: "center" }}>
@@ -136,8 +165,13 @@ function SomePie() {
 
 export default function ParticipatingClasses() {
     const [listOfClasses, setListOfClasses] = useState([]);
+    const [hideClass, setHideClass] = useState([]);
+
+    // const [is_load, setLoad] = useState(true);
+
+
     async function getAllClasses(project_id) {
-        let url = "/get_list_of_classes_in_project/" + project_id;
+        let url = "/get_series_for_pie/" + project_id;
 
         try {
             axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization")
@@ -145,7 +179,6 @@ export default function ParticipatingClasses() {
 
             if (res.status === 200 || res.status === 201) {
                 setListOfClasses(res.data);
-                console.log(res.data)
             } else {
             }
         } catch (err) {
@@ -157,22 +190,44 @@ export default function ParticipatingClasses() {
         getAllClasses(localStorage.getItem("last_project_id"));
     }, []);
     return (<>
-        <Card sx={{ borderRadius: "12px", width: "51.05vw", marginBottom: '1.85vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Card
+            sx={{
+                borderRadius: "12px",
+                width: "51.05vw",
+                marginBottom: '1.85vh',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
             <CardContent>
                 <Typography gutterBottom variant="h3" component="div" textAlign="center">
                     Участвующие классы
                 </Typography>
             </CardContent>
-            <SomePie />
+            <SomePie listOfClasses={listOfClasses} hideClass={hideClass} is_load={true} />
             <CardContent>
-                <List disablePadding sx={{ overflowY: "auto", maxHeight: "350px", '& ul': { padding: 0 }, width: "100%" }}>
+                <List disablePadding
+                    sx={{
+                        overflowY: "auto",
+                        maxHeight: "350px",
+                        '& ul': { padding: 0 },
+                        width: "100%"
+                    }}>
                     <Typography>Список классов</Typography>
                     {listOfClasses.map((item) =>
                         <ListItem
                             divider
                             key={item.id}
                         >
-                            <SomeClass id={item.id} class_name={item.class_name} class_color={item.class_color} class_description={item.class_description} />
+                            <SomeClass id={item.class_id}
+                                class_name={item.class_name}
+                                class_color={item.class_color}
+                                class_description={item.class_description}
+                                class_count={item.class_count}
+                                class_photo_count={item.class_photo_count}
+                                hideClass={hideClass}
+                                setHideClass={setHideClass}
+                            />
                         </ListItem>
                     )}
                 </List>
