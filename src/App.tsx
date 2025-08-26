de0e0c2bb5d5b5cd901714c9a78e26e056de9aac
lifeIp <molokeevartem@gmail.com>
2025-08-26 20:23:30 +0700

    Исправлен код для работы с хранилищем

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/Auth/Login/LoginPage';
-import RegistrationPage from './pages/Auth/Registration/RegistrationPage';
+
+import LoginPage from './Pages/Auth/Login/LoginPage';
+import RegistrationPage from './Pages/Auth/Registration/RegistrationPage';
 import NavigationPanel from './NavigationPanel/NavigationPanel';
-import MarkUpPage from './pages/MarkUpPage/MarkUpPage';
+import MarkUpPage from './Pages/MarkUpPage/MarkUpPage';
 
 import { observer } from 'mobx-react';
 import mainStore from "./store";
+
+
+
 
 function App() {
   return (
     <BrowserRouter>
     {mainStore.visibleNavigationPanel?(<NavigationPanel/>):(<></>)}
       <Routes>
+        <Route path="/upload" element={<LoginPage />}/>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/registration" element={<RegistrationPage />} />
         <Route path="/markup_fullscreen" element={<MarkUpPage/>} />
